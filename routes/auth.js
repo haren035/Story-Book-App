be7936f23a852a35a38c457eb80c5df483abdcc1
Haren Sah <haren3035@gmail.com>
2023-12-07 13:05:15 +1100

    logout setup

--- routes/auth.js
+++ routes/auth.js
@@ -17,4 +17,13 @@ router.get(
     res.redirect("/dashboard");
   }
 );
+
+// @desc    Logout User
+// @route    /auth/logout
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
 module.exports = router;
