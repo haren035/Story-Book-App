bb7bfdaba5f010748468bbf0dd75996290ffb697
Haren Sah <haren3035@gmail.com>
2023-12-11 17:12:51 +1100

    Add Story

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ require("./config/passport")(passport);
 connectDB();
 const app = express();
 
+//Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 //Logging
 if (process.env.NODE_ENV === "Development") {
   app.use(morgan("dev"));
@@ -54,6 +58,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //Route
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3000;
 
