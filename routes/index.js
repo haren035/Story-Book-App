acd3ed1c5052d419231478486cffadb4714fc30a
Haren Sah <haren3035@gmail.com>
2023-12-14 13:25:45 +1100

    public stories

--- routes/index.js
+++ routes/index.js
@@ -14,19 +14,30 @@ router.get("/", ensureGuest, (req, res) => {
 
 // @desc            dashboard
 // @route           GET/dashboard
-/*
-router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard", {
-    name: req.user.firstName,
-  });
-});
-*/
 
 router.get("/dashboard", ensureAuth, async (req, res) => {
   try {
     const stories = await Story.find({ user: req.user.id }).lean();
     res.render("dashboard", {
       name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
+
+// @desc    Show all stories
+// @route   GET /stories
+router.get("/", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+
+    res.render("stories/index", {
       stories,
     });
   } catch (err) {
